9265a4de46be537ec43072648dc344f991240bab
oli-gra <oliver.grant@gmail.com>
2019-04-07 23:01:53 +0100

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,29 @@
 class StudentsController < ApplicationController
+  def show
+    @student = get_params(:title, :room_number)
+    byebug
+  end
+
+  def index
+    @students = SchoolClass.all
+  end
+
+  def new
+    @student = SchoolClass.new
+  end
+
+  def create
+    @student = SchoolClass.new
+    @student.title = get_params(:title)
+    @student.room_number = get_params(:room_number)
+    @student.save
+    redirect_to school_class_path @student
+  end
+
+  private
+
+  def get_params(*args)
+    params.require(:student).permit(*args)
+  end
+
 end
